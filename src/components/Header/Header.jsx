257b233d3618b62a082a53c224cebf337328807f
ElenaGlushkova <amber_1@list.ru>
2020-12-15 11:01:53 +0300

    lesson 61 complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import classes from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
-        <header className={classes.header}>
-            <img src={'http://logok.org/wp-content/uploads/2014/05/Total-logo-earth-880x660.png'} width={'80px'} alt={''}/>
-        </header>
+        <div className={classes.header}>
+            <img src={'http://logok.org/wp-content/uploads/2014/05/Total-logo-earth-880x660.png'} width={'80px'}
+                 alt={''}/>
+            <div className={classes.loginBlock}>
+                {props.isAuth ?
+                    <div className={classes.authUser}>{props.login}</div>
+                    : <NavLink to={'/login'}>Login</NavLink>}
+            </div>
+        </div>
     );
 };
 
